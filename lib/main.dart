c7a80d620ec48d7c2c8ebe77fd398f30183ab41f
changs1505 <changs1505@gmail.com>
2023-12-04 13:21:41 +0900

    dataupload

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,20 @@
-
 import 'package:flutter/material.dart';
 import './style.dart' as style;
 import 'package:http/http.dart' as http;
-import 'dart:convert'; // dart에서 제공하는 기본 라이브러리 중 하나, 데이터를 인코딩, 디코딩 하는데 쓰임. (ex. json decode)
-import 'package:flutter/rendering.dart'; // 스크롤관련 함수들이 있는 라이브러리
+import 'dart:convert';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 
 void main() {
   runApp(MaterialApp(
-      theme: style.theme,
-      home: MyApp()
-  )
-  );
+    theme: style.theme,
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -24,8 +23,32 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var tab = 0;
   var data = [];
+  var userImage;
+  var userContent;
+  bool isLoading = true;
 
-  void addData(dynamic newData){
+  addMyData(){
+   var myData = {
+    'id': data.length,
+    'image': userImage,
+    'likes':5,
+    'date':'July 25',
+    'content': userContent,
+    'liked': false,
+    'user': 'John Kim'
+   };
+   setState(() {
+     data.insert(0, myData);
+   });
+  }
+
+  setUserContent(a){
+    setState(() {
+      userContent = a;
+    });
+  }
+
+  void addData(dynamic newData) {
     setState(() {
       data.add(newData);
     });
@@ -34,17 +57,23 @@ class _MyAppState extends State<MyApp> {
   getData() async {
     try {
       var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
-
       if (result.statusCode == 200) {
         var result2 = jsonDecode(result.body);
         setState(() {
           data = result2;
+          isLoading = false;
         });
       } else {
         print('서버 응답이 성공하지 않았습니다. 응답 코드: ${result.statusCode}');
+        setState(() {
+          isLoading = false;
+        });
       }
     } catch (error) {
       print('서버 통신 중 에러가 발생했습니다: $error');
+      setState(() {
+        isLoading = false;
+      });
     }
   }
 
@@ -54,7 +83,6 @@ class _MyAppState extends State<MyApp> {
     getData();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,30 +90,47 @@ class _MyAppState extends State<MyApp> {
         title: Text('Instagram'),
         actions: [
           IconButton(
-            icon:Icon(Icons.add_box_outlined),
-            onPressed: (){
+            icon: Icon(Icons.add_box_outlined),
+            onPressed: () async {
+              var picker = ImagePicker();
+              var image = await picker.pickImage(source: ImageSource.gallery); // 카메라로 바꾸고싶으면 .camera
+              if (image != null) {
+                setState((){
+                  userImage = File(image.path);
+                });
+              }
               Navigator.push(context,
-                MaterialPageRoute(builder: (c) => Upload() )
-              ); //새 페이지 띄우기: onPressed 클릭 시 페이지 이동
+                MaterialPageRoute(builder: (c) => Upload(
+                  userImage : userImage,
+                  setUserContent : setUserContent,
+                  addMyData : addMyData,
+                ))
+              );
             },
             iconSize: 30,
           )
         ],
       ),
-      body: [Home(data : data, addData: addData,), Text('샵'), Text('내정보'),][tab],
-
+      body: isLoading
+          ? Center(
+        child: CircularProgressIndicator(),
+      )
+          : [Home(data: data, addData: addData,), Text('샵'), Text('내정보')][tab],
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
-        onTap: (i){
+        onTap: (i) {
           setState(() {
             tab = i;
           });
         },
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: '홈', tooltip: '홈'),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined), label: '샵', tooltip: '샵'),
-          BottomNavigationBarItem(icon: Icon(Icons.person),label: '내정보', tooltip: '내정보'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), label: '홈', tooltip: '홈'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_bag_outlined), label: '샵', tooltip: '샵'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), label: '내정보', tooltip: '내정보'),
         ],
       ),
     );
@@ -95,6 +140,7 @@ class _MyAppState extends State<MyApp> {
 class Home extends StatefulWidget {
   const Home({Key? key, required this.data, required this.addData})
       : super(key: key);
+
   final List<dynamic> data;
   final Function(dynamic) addData;
 
@@ -103,11 +149,11 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  //23.12.01 scroll 작업
   var scroll = ScrollController();
 
   void getMore() async {
-    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/more1.json'));
+    var result =
+    await http.get(Uri.parse('https://codingapple1.github.io/app/more1.json'));
     var result2 = jsonDecode(result.body);
     widget.addData(result2);
   }
@@ -116,38 +162,39 @@ class _HomeState extends State<Home> {
   void initState() {
     super.initState();
     scroll.addListener(() {
-      if(scroll.position.pixels == scroll.position.maxScrollExtent){
+      if (scroll.position.pixels == scroll.position.maxScrollExtent) {
         print('맨 밑까지 스크롤 되었음');
         getMore();
       }
     });
   }
 
-
-
-
-  
   @override
   Widget build(BuildContext context) {
-    //print(widget.data); 데이터 출력 테스트
-    if(widget.data.isNotEmpty){
-      return ListView.builder(itemCount: widget.data.length, controller: scroll, itemBuilder: (c, i) { //변수 선언
-        return Column(
+    if (widget.data.isNotEmpty) {
+      return ListView.builder(
+        itemCount: widget.data.length,
+        controller: scroll,
+        itemBuilder: (c, i) {
+          return Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Image.network('${widget.data[i]['image']}'),
+              widget.data[i]['image'].runtimeType == String
+                  ? Image.network(widget.data[i]['image'])
+                  : Image.file(widget.data[i]['image']),  //3항 연산
               Text('ID: ${widget.data[i]['id']}'),
-              Text('Date: ${widget.data[i]['date']}'), // 예를 들어, 날짜를 문자열로 가정
+              Text('Date: ${widget.data[i]['date']}'),
               Row(
                 children: [
-                  Icon(Icons.favorite_border, color: Colors.black), // 하트 아이콘
-                  Text('${widget.data[i]['likes']}'), // 좋아요 수
+                  Icon(Icons.favorite_border, color: Colors.black),
+                  Text('${widget.data[i]['likes']}'),
                 ],
               ),
               Text('${widget.data[i]['content']}'),
-            ]
-        );
-      });
+            ],
+          );
+        },
+      );
     } else {
       return Text('로딩중');
     }
@@ -155,21 +202,36 @@ class _HomeState extends State<Home> {
 }
 
 class Upload extends StatelessWidget {
-  const Upload({Key? key}) : super(key: key);
-  @override
+  const Upload({Key? key, this.userImage, this.setUserContent, this.addMyData}) : super(key: key);
+  final userImage;
+  final setUserContent;
+  final addMyData;
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: (){
+            addMyData();
+          }, icon:Icon(Icons.send))
+        ]),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('이미지 업로드 화면'),
+          Container(
+            width: 200,
+            height: 200,
+            child: Image.file(userImage),
+          ),
+
+          TextField(onChanged: (text){ setUserContent(text); }),
           IconButton(
-              onPressed: (){
-                Navigator.pop(context);
-                },
-              icon: Icon(Icons.close)
+            onPressed: () {
+
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.close),
           ),
         ],
       ),
